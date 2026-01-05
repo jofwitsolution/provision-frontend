6de1dd56e5d10f31c2bc733990c3bfef1d4f2100
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2026-01-05 23:45:59 +0100

    add links

--- src/components/ui/JoinUs.jsx
+++ src/components/ui/JoinUs.jsx
@@ -3,12 +3,21 @@ import { Link } from "react-router-dom";
 
 const JoinUs = () => {
   return (
-    <div className="bg-join-us h-[50vh] bg-no-repeat bg-cover">
-      <div className="w-full h-full flex justify-center px-[3rem] md:w-[70%] items-center text-white text-[1.3rem] md:text-[2rem]">
-        <Link to="/contact">
+    <div className="bg-join-us h-[50vh] bg-no-repeat bg-cover px-2 md:px-4 pt-8">
+      <div className="w-full h-full items-center text-white text-[1.3rem] md:text-[2rem]">
+        <p className="text-center">
           Join ProVision Support and experience living that's about more than
           just a place; it's about community, support, and growth. Welcome home!
-        </Link>
+        </p>
+
+        <div className="mt-[1.9rem] text-white flex justify-center gap-4">
+          <Link to="/contact" className="text-primary underline">
+            Get in touch
+          </Link>
+          <Link to="/referrals" className="text-primaryAlt underline">
+            Make a referral
+          </Link>
+        </div>
       </div>
     </div>
   );
