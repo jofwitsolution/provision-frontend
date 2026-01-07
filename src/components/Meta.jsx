6d7d82c70f77f9a99a443807bf4a0832bec2507f
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2026-01-07 18:28:01 +0100

    update meta

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -4,9 +4,9 @@ import { Helmet } from "react-helmet";
 const Meta = ({
   title = "ProVision Support Services CIC",
   description = `
-  Celebrate Your Stay, Elevate Your Experience, and Empower Your Investment 
-  with ProVision. Discover Distinctive Accommodations Tailored to You, 
-  Where Sustainability Meets Unforgettable Memories. Your Journey Starts Here.
+    ProVision Support Services CIC is dedicated to providing exceptional accommodation and support services. 
+    We specialize in sustainable accommodations that empower individuals and communities. Explore our range of services and
+    discover how we can assist you in finding the perfect property solution.
   `,
   keywords = "provision, ProVision Support Services CIC, property, support service, ProVision Support Services CIC",
 }) => {
